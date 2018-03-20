e4011fe6061640ab0b4aa469b7653608ce697adb
mehraveh <mehraveh.ahmadi@yahoo.com>
2018-03-20 12:27:36 +0330

    Update Hazm.py

--- Hazm.py
+++ Hazm.py
@@ -5,8 +5,8 @@ import openpyxl
 def hazm(file_name):
     words = []
     words_count =[]
-    delete_list = [':',"?" ,"؟" ,"ک","ها","از","در","mehraveh_tma","bangtansv","MAminHP","!","ب","ی","یه","FWD","Photo","]","{","[","هاش",".","اگ","اگه","اگر","۱","۲","۳","۴","۵"
-                   ,"۶","۷","۸", "۹","…","نه", "ن" ,"و" ,"را"]
+    delete_list = [':',"?" ,"؟" ,"ک","ها","از","در","Mr_Mean","mehraveh_tma","bangtansv","MAminHP","!","ب","ی","یه","FWD","Photo","]","{","[","هاش",".","اگ","اگه","اگر","۱","۲","۳","۴","۵"
+                   ,"۶","۷","۸", "۹","…","نه", "ن" ,"و" ,"را" ,"که","این","در","برای","تو","من","او" ,"به","با","تا","یک","چی", "هر" ,"چون" ,"باشه","ولی","بعد","هم","یا","کلا","ای"]
 
     book = openpyxl.load_workbook('PersianWords.xlsx')
     sheet = book.active
@@ -17,6 +17,7 @@ def hazm(file_name):
 
     o = open(file_name.__str__()+"_tokenized.txt" ,"w")
     f = open(file_name.__str__()+"_cleaned.txt")
+    w = open(file_name.__str__()+"_wordmap_file.txt" , "w")
     normalizer = Normalizer()
     lemmatizer = Lemmatizer()
     for i in range(8032):
@@ -28,10 +29,12 @@ def hazm(file_name):
             word = fl.filtering(word,words_dict)
             word = lemmatizer.lemmatize(word.__str__())
             # print(word.__str__())
+            if word not in delete_list:
+                w.writelines(word.__str__()+""+"\n")
             if word not in words and not word in delete_list:
                 words.append(word.__str__())
                 words_count.append(1)
-                #o.writelines(word.__str__()+""+"\n")
+
             elif  word  in words:
                 words_count[words.index(word.__str__())] +=1
     print(words)
