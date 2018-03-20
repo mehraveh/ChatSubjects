0513357ad04b895745d4dd5a11b5756a73725b50
mehraveh <mehraveh.ahmadi@yahoo.com>
2018-03-20 12:26:13 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import Data_cleaning as dc
 import Hazm as h
+import wordmap as wm
 
-dc.cleaning("banafsh")
+
 dc.cleaning("Amin")
-h.hazm("banafsh")
+#h.hazm("banafsh")
 h.hazm("Amin")
+#wm.wordmap_genartor("banafsh")
+wm.wordmap_genartor("Amin")
+
+
 
